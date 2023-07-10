15f67227e61d0593019b7bb73af1f3f4b7359b85
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-07-10 04:07:04 +0000

    Add basic cli

--- cmd/root.go
+++ cmd/root.go
@@ -4,24 +4,43 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
 	"os"
 
+	"github.com/NguyenLe1605/gop4mini/pkg/utils"
 	"github.com/spf13/cobra"
 )
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "gop4mini",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A prototype for a P4 controller",
+	Long: `gop4mini is a P4 runtime controller that can install
+P4 rule into the bmv2 switches.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) { },
+	RunE: func(cmd *cobra.Command, args []string) error {
+		p4Info, err := cmd.Flags().GetString("p4info")
+		if err != nil {
+			return err
+		}	
+
+		bmv2Json, err := cmd.Flags().GetString("bmv2-json")
+		if err != nil {
+			return err
+		}
+
+		if err := utils.FileExists(p4Info); err != nil {
+				return fmt.Errorf("\np4info file not found: %s\nHave you run 'make'?", p4Info)
+		}
+
+		if err := utils.FileExists(bmv2Json); err != nil {
+			return fmt.Errorf("\nBMv2 JSON file not found: %s\nHave you run 'make'?", bmv2Json)
+	}
+
+		fmt.Println(p4Info, bmv2Json)
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -42,7 +61,8 @@ func init() {
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.PersistentFlags().String("p4info", "./build/advanced_tunnel.p4.p4info.txt", "p4info proto in text format from p4c")
+	rootCmd.PersistentFlags().String("bmv2-json", "./build/advanced_tunnel.json", "BMv2 JSON file from p4c")
 }
 
 
---
+++ pkg/utils/file.go
@@ -0,0 +1,11 @@
+package utils
+
+import "os"
+
+func FileExists(filename string) error {
+	_, err := os.Stat(filename)
+   if os.IsNotExist(err) {
+      return err
+   }
+	 return nil;
+}
